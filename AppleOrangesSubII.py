0f619c59f9ea54c83bc2928cc0efdb310e232538
JustinVillas <92616184+JustinVillas@users.noreply.github.com>
2021-11-12 20:18:14 +0800

    secon commit
    global function

--- AppleOrangesSubII.py
+++ AppleOrangesSubII.py
@@ -4,14 +4,18 @@
 # Display the total amount of the bought items.
 # The total amount is __________.
 
+def num_apples_oranges():
+    num_apples = int(input("Suki, Ilang apple ang gusto mo? "))
+    num_oranges = int(input("Suki, Bumili kana din ng orange? "))
+    return num_apples, num_oranges
+
 
 def total_amount(apple, orange):
     return int((apple * 20) + (orange * 25))
 
 
 # Ask the consumer the number of apples and oranges
-num_apples = int(input("Suki, Ilang apple ang gusto mo? "))
-num_oranges = int(input("Suki, Bumili kana din ng orange? "))
+num_apples, num_oranges = num_apples_oranges()
 # Compute and output the total amount
 total = total_amount(num_apples, num_oranges)
 # Display the apple and orange.
